fe9505ca826ddc92b2d55dc5175e36ce1360b539
Cameron Wong <cam@camdar.io>
2023-09-07 23:12:08 -0400

    fix inventory and add recurring boss logic
    Signed-off-by: Cameron Wong <cam@camdar.io>

--- worlds/fe8/fe8_rando.py
+++ worlds/fe8/fe8_rando.py
@@ -1,10 +1,12 @@
 import pkgutil
-import random
+# This is renamed to catch any uses of the non-multiworld random functions.
+import random as rng
+from rng import Random
 import json
 from collections import defaultdict
 from dataclasses import dataclass
 from typing import Any, Self, Optional
-from enum import Enum
+from enum import IntEnum
 
 
 WEAPON_DATA_JSON = "data/weapondata.json"
@@ -14,9 +16,17 @@ CHAPTER_UNIT_BLOCKS_JSON = "data/chapter_unit_blocks.json"
 CHAPTER_UNIT_SIZE = 20
 INVENTORY_INDEX = 0xC
 INVENTORY_SIZE = 0x4
+CHARACTER_TABLE_OFFSET = 0x803D30
 
+# TODO: there are other recurring bosses, like Lyon and Valter
 ORSON_5X = 0x42
 ORSON_BOSS = 0x6D
+VALTER_PROLOGUE = 0x43
+VALTER_BOSS = 0x45
+PABLO_10 = 0x4F
+PABLO_13 = 0x54
+LYON_17 = 0x40
+LYON_ENDGAME = 0x6C
 EIRIKA = 0x1
 EIRIKA_RAPIER_OFFSET = 0x9EF088
 
@@ -34,7 +44,7 @@ class UnitBlock:
         return UnitBlock(base=int(obj["base"], 16), count=obj["count"])
 
 
-class WeaponKind(Enum):
+class WeaponKind(IntEnum):
     SWORD = 0x00
     LANCE = 0x01
     AXE = 0x02
@@ -77,7 +87,7 @@ class WeaponKind(Enum):
         return None
 
 
-class WeaponRank(Enum):
+class WeaponRank(IntEnum):
     E = 0x1
     D = 0x1F
     C = 0x47
@@ -146,11 +156,11 @@ class FE8Randomizer:
     jobs_by_id: dict[int, JobData]
     promoted_jobs: list[JobData]
     unpromoted_jobs: list[JobData]
-    assigned_player_classes: dict[int, int]
-    random: random.Random
+    fixed_char_data: dict[int, int]
+    random: Random
     rom: bytearray
 
-    def __init__(self, rom: bytearray, random: random.Random):
+    def __init__(self, rom: bytearray, random: Random):
         self.random = random
         self.rom = rom
         self.unit_blocks = json.loads(
@@ -170,7 +180,7 @@ class FE8Randomizer:
         # TODO: handle these properly
         job_data = [job for job in job_data if job.usable_weapons]
 
-        self.assigned_player_classes = dict()
+        self.fixed_char_data = dict()
 
         self.weapons_by_id = {item.id: item for item in item_data}
         self.jobs_by_id = {job.id: job for job in job_data}
@@ -198,7 +208,7 @@ class FE8Randomizer:
             if weap.kind in job.usable_weapons
         ]
 
-        return random.choice(choices).id
+        return self.random.choice(choices).id
 
     def randomize_chapter_unit(self, data_offset: int) -> None:
         # We *could* read the full struct, but we only need a few individual
@@ -214,11 +224,13 @@ class FE8Randomizer:
         job = self.jobs_by_id[job_id]
         char = unit[0]
         # Affiliation = bits 1,2; unit is player if they're unset
-        is_player = not bool(unit[2] & 6)
+        is_player = not bool(unit[2] & 0b0110)
+        # Autolevel is LSB
+        autolevel = unit[2] & 1
         inventory = unit[INVENTORY_INDEX : INVENTORY_INDEX + INVENTORY_SIZE]
 
-        if char in self.assigned_player_classes:
-            new_job = self.assigned_player_classes[char]
+        if char in self.fixed_char_data:
+            new_job = self.fixed_char_data[char]
         else:
             new_job_pool = (
                 self.promoted_jobs if job.is_promoted else self.unpromoted_jobs
@@ -226,10 +238,16 @@ class FE8Randomizer:
             new_job = self.random.choice(new_job_pool)
 
             if is_player:
-                self.assigned_player_classes[char] = new_job
+                self.fixed_char_data[char] = new_job
 
         if char == ORSON_BOSS:
-            new_job = self.assigned_player_classes[ORSON_5X]
+            new_job = self.fixed_char_data[ORSON_5X]
+        if char == VALTER_BOSS:
+            new_job = self.fixed_char_data[VALTER_PROLOGUE]
+        if char == PABLO_13:
+            new_job = self.fixed_char_data[PABLO_10]
+        if char == LYON_ENDGAME:
+            new_job = self.fixed_char_data[LYON_17]
 
         new_inventory = [self.select_new_item(new_job, item) for item in inventory]
 
@@ -237,12 +255,38 @@ class FE8Randomizer:
         for i, item_id in enumerate(new_inventory):
             self.rom[data_offset + INVENTORY_INDEX + i] = item_id
 
+        # If an NPC isn't autoleveled, it's probably a boss or important NPC of
+        # some kind, so we should force its weapon levels in the character
+        # table.
+        if not is_player and autolevel:
+            for item_id in new_inventory:
+                if item_id not in self.weapons_by_id:
+                    continue
+                boss_data_addr = CHARACTER_TABLE_OFFSET + char * 52
+                weapon = self.weapons_by_id[item_id]
+                boss_wrank_addr = boss_data_addr + 20 + weapon.kind
+                rank = self.rom[boss_wrank_addr]
+                self.rom[boss_wrank_addr] = max(rank, weapon.rank)
+
+    # TODO: logic
+    #   - Valter must be holding a weapon
+    #   - at least one of Eirika or Seth must be able to fight
+    #   - at least one of Ephraim/Forde/Kyle/Orson must be able to fight
+    #   - at least one of L'Arachel or Dozla must be able to fight
+    #   - Nudge Cormag to 5,15 (pre) and 6,13 (post) in Eirika 13
+    #   - Nudge Cormag to 11,12 in Ephraim 10
+    #   - Nudge Tana to 0,5
+    #   - Nudge the chapter 2 brigands
+    #
+    # TODO: polish
+    #   - NPCs that appear frequently in cutscenes (Caellach, Glen, Riev, etc)
+    #     should have the same class every time.
     def apply_changes(self) -> None:
         for block in self.unit_blocks:
             for i in range(block.count):
                 self.randomize_chapter_unit(block.base + i * CHAPTER_UNIT_SIZE)
 
-        eirika_job = self.assigned_player_classes[EIRIKA]
+        eirika_job = self.fixed_char_data[EIRIKA]
         # We give a random C-ranked weapon to Eirika to simulate the Rapier.
         new_rapier = self.select_new_item(eirika_job, STEEL_BLADE)
         self.rom[EIRIKA_RAPIER_OFFSET] = new_rapier
