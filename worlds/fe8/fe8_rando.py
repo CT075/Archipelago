ddb378204f609e0eec2cacf82083d3839f1beea8
Cameron Wong <cam@camdar.io>
2023-09-10 00:48:19 -0400

    make logic better
    Signed-off-by: Cameron Wong <cam@camdar.io>

--- worlds/fe8/fe8_rando.py
+++ worlds/fe8/fe8_rando.py
@@ -6,33 +6,63 @@ from random import Random
 import json
 from collections import defaultdict
 from dataclasses import dataclass
-from typing import Any, Self, Optional, NamedTuple, Union
+from typing import Any, TypeVar, Callable, Union, Optional
 from enum import IntEnum
 
 # CR cam: Maybe these should go into [constants]?
 
-WEAPON_DATA_JSON = "data/weapondata.json"
-JOB_DATA_JSON = "data/jobdata.json"
-CHARACTERS_JSON = "data/characters.json"
-CHAPTER_UNIT_BLOCKS_JSON = "data/chapter_unit_blocks.json"
+WEAPON_DATA = "data/weapondata.json"
+JOB_DATA = "data/jobdata.json"
+CHARACTERS = "data/characters.json"
+CHAPTER_UNIT_BLOCKS = "data/chapter_unit_blocks.json"
+
+T = TypeVar("T")
+
+
+def fetch_json(
+    path: str, object_hook: Optional[Callable[[dict[str, Any]], T]] = None
+) -> Any:
+    data = pkgutil.get_data(__name__, path)
+    if data is None:
+        raise FileNotFoundError
+    return json.loads(data.decode("utf-8"), object_hook=object_hook)
+
 
 CHAPTER_UNIT_SIZE = 20
 INVENTORY_INDEX = 0xC
 INVENTORY_SIZE = 0x4
 CHARACTER_TABLE_OFFSET = 0x803D30
+CHARACTER_SIZE = 52
+CHARACTER_WRANK_BASE = 20
 
 EIRIKA_RAPIER_OFFSET = 0x9EF088
+BONE_COORDS_OFFSET = 0x9F0310
 
 STEEL_BLADE = 0x6
 FLUX = 0x45
 
 
-@dataclass
+def encode_reda_coords(x: int, y: int) -> int:
+    return y << 6 | x
+
+
 class UnitBlock:
     name: str
     base: int
     count: int
-    tags: list[str]
+
+    # Currently, the names of blocks in `chapter_unit_blocks.json` are all
+    # automatically generated from chapter event disassembly and are tagged
+    # with any relevant information about the block. However,
+    logic: defaultdict[int, dict[str, Any]]
+
+    def __init__(
+        self, name: str, base: int, count: int, logic: dict[int, dict[str, Any]]
+    ):
+        self.name = name
+        self.base = base
+        self.count = count
+        self.logic = defaultdict(dict, {int(k): v for k, v in logic.items()})
 
 
 class WeaponKind(IntEnum):
@@ -46,36 +76,37 @@ class WeaponKind(IntEnum):
     DARK = 0x07
     ITEM = 0x09
     MONSTER_WEAPON = 0x0B
-    RINGS = 0x0C
+    RING = 0x0C
     DRAGONSTONE = 0x11
 
     @classmethod
-    def of_str(cls, s: str) -> Optional[Self]:
-        if s == "Sword":
-            return WeaponKind.SWORD
-        elif s == "Lance":
-            return WeaponKind.LANCE
-        elif s == "Axe":
-            return WeaponKind.AXE
-        elif s == "Bow":
-            return WeaponKind.BOW
-        elif s == "Staff":
-            return WeaponKind.STAFF
-        elif s == "Anima":
-            return WeaponKind.ANIMA
-        elif s == "Light":
-            return WeaponKind.LIGHT
-        elif s == "Dark":
-            return WeaponKind.DARK
-        elif s == "Item":
-            return WeaponKind.ITEM
-        elif s == "Monster Weapon":
-            return WeaponKind.MONSTER_WEAPON
-        elif s == "Rings":
-            return WeaponKind.RINGS
-        elif s == "Dragonstone":
-            return WeaponKind.DRAGONSTONE
-        return None
+    def of_str(cls, s: str) -> "WeaponKind":
+        match s:
+            case "Sword":
+                return WeaponKind.SWORD
+            case "Lance":
+                return WeaponKind.LANCE
+            case "Axe":
+                return WeaponKind.AXE
+            case "Bow":
+                return WeaponKind.BOW
+            case "Staff":
+                return WeaponKind.STAFF
+            case "Anima":
+                return WeaponKind.ANIMA
+            case "Light":
+                return WeaponKind.LIGHT
+            case "Dark":
+                return WeaponKind.DARK
+            case "Item":
+                return WeaponKind.ITEM
+            case "Monster Weapon":
+                return WeaponKind.MONSTER_WEAPON
+            case "Ring":
+                return WeaponKind.RING
+            case "Dragonstone":
+                return WeaponKind.DRAGONSTONE
+        raise ValueError
 
 
 class WeaponRank(IntEnum):
@@ -87,20 +118,21 @@ class WeaponRank(IntEnum):
     S = 0xFB
 
     @classmethod
-    def of_str(cls, s: str) -> Optional[Self]:
-        if s == "E":
-            return WeaponRank.E
-        elif s == "D":
-            return WeaponRank.D
-        elif s == "C":
-            return WeaponRank.C
-        elif s == "B":
-            return WeaponRank.B
-        elif s == "A":
-            return WeaponRank.A
-        elif s == "S":
-            return WeaponRank.S
-        return None
+    def of_str(cls, s: str) -> "WeaponRank":
+        match s:
+            case "E":
+                return WeaponRank.E
+            case "D":
+                return WeaponRank.D
+            case "C":
+                return WeaponRank.C
+            case "B":
+                return WeaponRank.B
+            case "A":
+                return WeaponRank.A
+            case "S":
+                return WeaponRank.S
+        raise ValueError
 
 
 @dataclass
@@ -134,7 +166,9 @@ class JobData:
             id=obj["id"],
             name=obj["name"],
             is_promoted=obj["is_promoted"],
-            usable_weapons=[WeaponKind.of_str(kind) for kind in obj["usable_weapons"]],
+            usable_weapons=set(
+                WeaponKind.of_str(kind) for kind in obj["usable_weapons"]
+            ),
             tags=obj["tags"],
         )
 
@@ -181,12 +215,23 @@ class CharacterStore:
         return name in self.jobs_by_name
 
 
+def weapon_valid(weapon: WeaponData, logic: dict[str, Any]) -> bool:
+    if "must_fight" in logic and weapon.kind in [
+        WeaponKind.ITEM,
+        WeaponKind.STAFF,
+        WeaponKind.RING,
+    ]:
+        return False
+
+    return True
+
+
 # TODO: ensure that all the progression weapons are usable
 class FE8Randomizer:
-    unit_blocks: dict[str, UnitBlock]
+    unit_blocks: dict[str, list[UnitBlock]]
     weapons_by_id: dict[int, WeaponData]
     weapons_by_rank: dict[WeaponRank, list[WeaponData]]
-    recurring_characters: CharacterStore
+    character_store: CharacterStore
     jobs_by_id: dict[int, JobData]
     promoted_jobs: list[JobData]
     unpromoted_jobs: list[JobData]
@@ -196,32 +241,24 @@ class FE8Randomizer:
     def __init__(self, rom: bytearray, random: Random):
         self.random = random
         self.rom = rom
-        unit_blocks = json.loads(
-            pkgutil.get_data(__name__, CHAPTER_UNIT_BLOCKS_JSON).decode("utf-8"),
-        )
+        unit_blocks = fetch_json(CHAPTER_UNIT_BLOCKS)
+
         self.unit_blocks = {
             name: [UnitBlock(**block) for block in blocks]
             for name, blocks in unit_blocks.items()
         }
 
-        item_data = json.loads(
-            pkgutil.get_data(__name__, WEAPON_DATA_JSON).decode("utf-8"),
-            object_hook=WeaponData.of_object,
-        )
+        item_data = fetch_json(WEAPON_DATA, object_hook=WeaponData.of_object)
 
-        job_data = json.loads(
-            pkgutil.get_data(__name__, JOB_DATA_JSON).decode("utf-8"),
+        job_data = fetch_json(
+            JOB_DATA,
             object_hook=JobData.of_object,
         )
 
         # TODO: handle these properly
         job_data = [job for job in job_data if job.usable_weapons]
 
-        self.recurring_characters = CharacterStore(
-            json.loads(
-                pkgutil.get_data(__name__, CHARACTERS_JSON).decode("utf-8"),
-            )
-        )
+        self.character_store = CharacterStore(fetch_json(CHARACTERS))
 
         self.weapons_by_id = {item.id: item for item in item_data}
         self.jobs_by_id = {job.id: job for job in job_data}
@@ -237,52 +274,7 @@ class FE8Randomizer:
         # Dark has no E-ranked weapons, so we add Flux
         self.weapons_by_rank[WeaponRank.E].append(self.weapons_by_id[FLUX])
 
-    def unit_must_fight(self, offset: int, char_id: int, chapter_name: str) -> bool:
-        char = self.recurring_characters.lookup_name(char_id)
-
-        if char is None:
-            return False
-
-        # Prologue Valter must be able to fight Seth, and Seth must be able to
-        # fight back
-        if chapter_name == "Prologue":
-            if char in ["Seth", "Valter"]:
-                return True
-
-        # At least one of Ephraim, Forde, Kyle and Orson should be able to
-        # fight. Orson is the natural choice, as he cannot be randomized into
-        # a staff-only class anyway.
-        #
-        # CR cam: In theory, if Ephraim/Forde/Kyle are all combatants, Orson
-        # may not have enough weapon uses to get through 5x..
-        if chapter_name == "Ch5x":
-            if char == "Orson":
-                return True
-
-        # Glen must be able to fight Valter
-        if char == "Glen":
-            return True
-
-        # Dozla and L'arachel appear as NPCs a few times; he should be able to
-        # fight in both of them.
-        if char == "Dozla":
-            return True
-
-        return False
-
-    def select_new_inventory(
-        self, job: JobData, items: bytes, at_least_one_weapon: bool
-    ) -> list[int]:
-        return [
-            # If `at_least_one_weapon`, we force the first item in inventory to
-            # be a weapon. In theory, this could be a vulnerary or some such,
-            # but all cases in which this would matter have a proper weapon
-            # there.
-            self.select_new_item(job, item_id, at_least_one_weapon and i != 0)
-            for i, item_id in enumerate(items)
-        ]
-
-    def select_new_item(self, job: JobData, item_id: int, force_weapon: bool):
+    def select_new_item(self, job: JobData, item_id: int, logic: dict[str, Any]):
         if item_id not in self.weapons_by_id:
             return item_id
 
@@ -291,15 +283,19 @@ class FE8Randomizer:
         choices = [
             weap
             for weap in self.weapons_by_rank[weapon_attrs.rank]
-            if weap.kind in job.usable_weapons
+            if weap.kind in job.usable_weapons and weapon_valid(weap, logic)
         ]
-
-        if force_weapon:
-            choices = [choice for choice in choices if choice.kind != WeaponKind.STAFF]
 
         return self.random.choice(choices).id
 
-    def randomize_chapter_unit(self, data_offset: int, chapter_name: str) -> None:
+    def select_new_inventory(
+        self, job: JobData, items: bytes, logic: dict[str, Any]
+    ) -> list[int]:
+        return [
+            self.select_new_item(job, item_id, logic) for i, item_id in enumerate(items)
+        ]
+
+    def randomize_chapter_unit(self, data_offset: int, logic: dict[str, Any]) -> None:
         # We *could* read the full struct, but we only need a few individual
         # bytes, so we may as well extract them ad-hoc.
         unit = self.rom[data_offset : data_offset + CHAPTER_UNIT_SIZE]
@@ -313,24 +309,24 @@ class FE8Randomizer:
         job = self.jobs_by_id[job_id]
         char = unit[0]
         # Affiliation = bits 1,2; unit is player if they're unset
-        is_player = not bool(unit[2] & 0b0110)
+        is_player = not bool(unit[3] & 0b0110)
         # Autolevel is LSB
-        autolevel = unit[2] & 1
+        autolevel = unit[3] & 1
         inventory = unit[INVENTORY_INDEX : INVENTORY_INDEX + INVENTORY_SIZE]
 
-        if char in self.recurring_characters:
-            new_job = self.recurring_characters[char]
+        if char == 0x45:
+            new_job = self.jobs_by_id[27]
+        elif char in self.character_store:
+            new_job = self.character_store[char]
         else:
             new_job_pool = (
                 self.promoted_jobs if job.is_promoted else self.unpromoted_jobs
             )
             new_job = self.random.choice(new_job_pool)
 
-            self.recurring_characters[char] = new_job
+            self.character_store[char] = new_job
 
-        new_inventory = self.select_new_inventory(
-            new_job, inventory, self.unit_must_fight(data_offset, char, chapter_name)
-        )
+        new_inventory = self.select_new_inventory(new_job, inventory, logic)
 
         self.rom[data_offset + 1] = new_job.id
         for i, item_id in enumerate(new_inventory):
@@ -339,38 +335,44 @@ class FE8Randomizer:
         # If an NPC isn't autoleveled, it's probably a boss or important NPC of
         # some kind, so we should force its weapon levels in the character
         # table.
-        if not is_player and autolevel:
+        if not is_player and not autolevel:
             for item_id in new_inventory:
                 if item_id not in self.weapons_by_id:
                     continue
-                boss_data_addr = CHARACTER_TABLE_OFFSET + char * 52
+                boss_data_addr = CHARACTER_TABLE_OFFSET + char * CHARACTER_SIZE
                 weapon = self.weapons_by_id[item_id]
-                boss_wrank_addr = boss_data_addr + 20 + weapon.kind
+                boss_wrank_addr = boss_data_addr + CHARACTER_WRANK_BASE + weapon.kind
                 rank = self.rom[boss_wrank_addr]
                 self.rom[boss_wrank_addr] = max(rank, weapon.rank)
 
-    def randomize_block(self, block: UnitBlock, chapter_name: str):
+    def randomize_block(self, block: UnitBlock):
         for i in range(block.count):
             self.randomize_chapter_unit(
-                block.base + i * CHAPTER_UNIT_SIZE, chapter_name=chapter_name
+                block.base + i * CHAPTER_UNIT_SIZE, block.logic[i]
             )
 
+    def apply_cutscene_fixes(self):
+        # Eirika's Rapier is given in a cutscene at the start of the chapter,
+        # rather than being in her inventory
+        eirika_job = self.character_store["Eirika"]
+        new_rapier = self.select_new_item(eirika_job, STEEL_BLADE, {})
+        self.rom[EIRIKA_RAPIER_OFFSET] = new_rapier
+
+        # The chapter 2 boss moves to an inaccessible mountain tile after his
+        # pre-map dialogue.
+        self.rom[BONE_COORDS_OFFSET] = 10
+        self.rom[BONE_COORDS_OFFSET] = 8
+
     # TODO: logic
-    #   - Valter must be holding a weapon
     #   - at least one of L'Arachel or Dozla must be able to fight
     #   - Nudge Cormag to 5,15 (pre) and 6,13 (post) in Eirika 13
     #   - Nudge Cormag to 11,12 in Ephraim 10
     #   - Nudge Tana to 0,5
-    #   - Nudge the chapter 2 brigands
     #   - Saleh/Innes and Duessel/Knoll need to be able to fight in Ch15
     #   - Flying Duessel vs enemy archers in that Ephraim map may be unbeatable
     def apply_changes(self) -> None:
-        for chapter_name, chapter in self.unit_blocks.items():
+        for _chapter_name, chapter in self.unit_blocks.items():
             for block in chapter:
-                self.randomize_block(block, chapter_name=chapter_name)
+                self.randomize_block(block)
 
-        eirika_job = self.recurring_characters["Eirika"]
-
-        # We give a random C-ranked weapon to Eirika to simulate the Rapier.
-        new_rapier = self.select_new_item(eirika_job, STEEL_BLADE, False)
-        self.rom[EIRIKA_RAPIER_OFFSET] = new_rapier
+        self.apply_cutscene_fixes()
