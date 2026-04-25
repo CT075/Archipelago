88f6dda4eacb726cfddcadbc1f1e659a8a5d5300
phidemic-phi <phidemic@gmail.com>
2026-04-24 17:34:55 -0700

    Changes requested

--- worlds/fe8/rom.py
+++ worlds/fe8/rom.py
@@ -55,7 +55,7 @@ class FE8PatchExtension(APPatchExtension):
 
         randomizer.apply_base_changes()
         
-        if config["Secure_Ross"]== 2:
+        if config["Secure_Ross"] == 2:
             randomizer.early_flier()
 
         if config["shuffle_skirmish_tables"]:
