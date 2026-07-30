6900744e50a340e718ff38a71197899d472c45bd
phidemic-phi <phidemic@gmail.com>
2026-07-29 21:48:54 -0700

    grammer update

--- worlds/fe8/options.py
+++ worlds/fe8/options.py
@@ -139,7 +139,7 @@ class ForceHealer(Range):
     This is done using Eirika route recruitment order.
     """
 
-    display_name = "Force healer in first X units"
+    display_name = "Guarantee a healer"
     range_start = 0
     range_end = 34
     default = 0
