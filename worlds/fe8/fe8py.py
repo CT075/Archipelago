0273d3cd553ed6ac6dc92eb6f8f071ecd23f8376
phidemic-phi <phidemic@gmail.com>
2026-08-07 03:19:55 -0700

    optimization of forced healer

--- worlds/fe8/fe8py.py
+++ worlds/fe8/fe8py.py
@@ -299,6 +299,7 @@ class JobType(IntEnum):
     RANGED = 1
     FLIER = 2
     LOCKPICK = 3
+    HEALER = 4
 
 
 @dataclass
@@ -562,6 +563,11 @@ class FE8Randomizer:
                     self.jobs_pools[job.is_promoted][JobRace.ALL][
                         JobType.RANGED
                     ].append(job)
+                if "healer" in job.tags:
+                    self.jobs_pools[job.is_promoted][Race][JobType.HEALER].append(job)
+                    self.jobs_pools[job.is_promoted][JobRace.ALL][
+                        JobType.HEALER
+                    ].append(job)
                 self.jobs_pools[job.is_promoted][JobRace.ALL][JobType.ANY].append(job)
                 self.jobs_pools[job.is_promoted][Race][JobType.ANY].append(job)
 
@@ -628,9 +634,6 @@ class FE8Randomizer:
         # job is invalid if it has any of the tags in notags
         if notags and notags & job.tags:
             return False
-        if "must_heal" in logic and logic["must_heal"]:
-            if "healer" not in job.tags:
-                return False
         if "must_fight" in logic and logic["must_fight"]:
             if "cannot_fight" in job.tags:
                 return False
@@ -829,6 +832,8 @@ class FE8Randomizer:
             # but should never need to
             if "must_fly" in logic and logic["must_fly"]:
                 Rules = JobType.FLIER
+            elif "must_heal" in logic and logic["must_heal"]:
+                Rules = JobType.HEALER
             else:
                 Rules = JobType.ANY
 
